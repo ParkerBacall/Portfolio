a9538e9479ea10167e0c32921e78892d1954084b
Parker Bacall <parkerbacall@Parkers-MBP.lan>
2022-12-25 22:37:48 -0500

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>Everyone's a critic</h1>
+        <h1>Everyone's a Critic</h1>
       </main>
     </>
   )
