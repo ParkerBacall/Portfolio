10d1581a6cb6a764593cbc950967d8467da3d8ec
Parker Bacall <parkerbacall@Parkers-MBP.lan>
2023-05-12 22:03:30 -0700

    title overlay

--- components/GridImage.jsx
+++ components/GridImage.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
-export default function GridImage({ image, title }) {
-    
+export default function GridImage({ image, title, link }) {
+
+    const [isHighlighted, setIsHghlighted] = useState(false)
+
+    const handleHeightlighted = () => {
+        setIsHghlighted(true)
+    }
+
+    const handleMouseOut = () => {
+        setIsHghlighted(false)
+    }
+
     return (
-            <div className="h-[300px]">
+            <div className="h-[300px] relative" onMouseEnter={handleHeightlighted} onMouseLeave={handleMouseOut}
+            >
+                <Link href={link ? link : '/'}>
+                <div className={`z-10 absolute bg-black/40 h-full w-full items-center justify-center ${isHighlighted ?'flex': 'hidden'}`}>
+                    <span className='text-white'>{title}</span>
+                </div>
                 <Image src={image}
                     width={500}
                     height={500}
                     alt={title}
                     style={{objectFit: "cover",
                     width: "100%",
-                    height: "100%"
+                    height: "100%",
                 }}
                 />
+                </Link>
             </div>
     )
 }
--- components/ImageGrid.jsx
+++ components/ImageGrid.jsx
@@ -8,32 +8,32 @@ export default function ImageSlideshow() {
 
             <GridImage
                 image='https://res.cloudinary.com/dpdnba5be/image/upload/v1680743856/thisWillEND_axjmi6.png'
-                title='This will end' />
-
+                title='This will end'
+                href="/" />
             <GridImage
                 image='https://res.cloudinary.com/dpdnba5be/image/upload/v1680493838/meWithart_sdmybv.png'
-                title='Artist with Carrion painting' />
+                title='Artist with Carrion painting' href="/" />
 
             <GridImage
                 image='https://res.cloudinary.com/dpdnba5be/image/upload/v1680563695/FALLEN_szv0bn.png'
-                title='Fallen Mural' />
+                title='Fallen Mural' href="/" />
 
             <GridImage
                 image='https://res.cloudinary.com/dpdnba5be/image/upload/v1680563697/SEPUKKU_bqqpo0.png'
-                title='Seppuku Mural' />
+                title='Seppuku Mural' href="/" />
 
             <GridImage
                 image='https://res.cloudinary.com/dpdnba5be/image/upload/v1680149030/final4_amutft.png'
-                title='Skull Sqaure' />
+                title='Skull Sqaure' href="/" />
 
 
             <GridImage
                 image='https://res.cloudinary.com/dpdnba5be/image/upload/v1680149027/flowerskull_eo9dbf.png'
-                title='Skull Sqaure' />
+                title='Skull Sqaure' href="/" />
 
             <GridImage
                 image='https://res.cloudinary.com/dpdnba5be/image/upload/v1680149032/totem_vkzr1y.png'
-                title='Skull Sqaure' />
+                title='Skull Sqaure' href="/"/>
         </div>
     )
 }
