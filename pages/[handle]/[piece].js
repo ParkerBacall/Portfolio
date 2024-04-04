45ed4a76f442f2105faf80ced9ea37339345f4da
Parker Bacall <pbacall2@gmail.com>
2024-04-03 18:30:05 -0700

    fix page loading bug?

--- pages/[handle]/[piece].js
+++ pages/[handle]/[piece].js
@@ -14,34 +14,17 @@ export default function DynamicPage() {
     const [imagesArray, setImagesArray] = useState([])
 
     useEffect(() => {
-        setImagesArray(getImagesByHandle(query.handle), [...imagesArray])
-    }, [query])
-
-    useEffect(() => {
+        setImagesArray([])
+        setImagesArray(getImagesByHandle(query.handle))
         imagesArray.forEach((image, index) => {
             if (image.title === pageData?.title) {
                 setCurrentIndex(index)
             }
         })
-
-    }, [pageData, imagesArray])
+    }, [query])
 
     let prevHandle, nextHandle, category = ''
     let secondaryCategory = null
-
-      const keyStr =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
-  
-  const triplet = (e1, e2, e3) =>
-    keyStr.charAt(e1 >> 2) +
-    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-    keyStr.charAt(e3 & 63)
-  
-  const rgbDataURL = (r, g, b) =>
-    `data:image/gif;base64,R0lGODlhAQABAPAA${
-      triplet(0, r, g) + triplet(b, 255, 255)
-    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
 
     if (useRouter().isReady) {
         prevHandle = '/' + query.handle + imagesArray.sort((a, b) => b.year - a.year)[currentIndex - 1]?.handle
